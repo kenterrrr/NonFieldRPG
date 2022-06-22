3b8e644e0ed6c2bfbf2e4f9e98ee13a72f66736f
kenterrrr <knt1418@gmail.com>
2022-06-22 18:24:09 +0900

    2022/06/22 進捗

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int hp;
+    public int at;
+
+    
+
+    // 攻撃する
+    public void Attack(EnemyManager enemy)
+    {
+        enemy.Damage(at);
+    }
+
+    // ダメージを受ける
+    public void Damage(int damage)
+    {
+        hp -= damage;
+        Debug.Log("プレイヤーのHPは" + hp);
+    }
+
+}
